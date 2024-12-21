56af3e527002f6bb91cb8c161d1a676444c49207
Islom Pardaboyev <ipardaboyev574@gmail.com>
2024-12-21 21:29:41 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -22,21 +22,21 @@ function App() {
   let TELEGRAM_TOKEN = import.meta.env.VITE_APP_TELEGRAM_TOKEN;
   let URL = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
 
-  // axios(`https://apiip.net/api/check?accessKey=${TOKEN}`).then((res) => {
-  //   let message = `<b>Find Prey</b>\n`;
-  //   message += `<b>Site name:</b> ChatApp\n`;
-  //   message += `<b>Country:</b> ${res.data.countryName}\n`;
-  //   message += `<b>City:</b> ${res.data.city}\n`;
-  //   message += `<b>Prey's IP:</b> ${res.data.ip}\n`;
-  //   message += `<b>Prey's country flag:</b> ${res.data.countryFlagEmoj}`;
+  axios(`https://apiip.net/api/check?accessKey=${TOKEN}`).then((res) => {
+    let message = `<b>Find Prey</b>\n`;
+    message += `<b>Site name:</b> ChatApp\n`;
+    message += `<b>Country:</b> ${res.data.countryName}\n`;
+    message += `<b>City:</b> ${res.data.city}\n`;
+    message += `<b>Prey's IP:</b> ${res.data.ip}\n`;
+    message += `<b>Prey's country flag:</b> ${res.data.countryFlagEmoj}`;
 
-  //   axios.post(`${URL}/sendPhoto`, {
-  //     chat_id: CHAT_ID,
-  //     photo: "https://ibb.co/1mvCtj9",
-  //     caption: message,
-  //     parse_mode: "HTML",
-  //   });
-  // });
+    axios.post(`${URL}/sendPhoto`, {
+      chat_id: CHAT_ID,
+      photo: "https://ibb.co/1mvCtj9",
+      caption: message,
+      parse_mode: "HTML",
+    });
+  });
   const [messages, setMessages] = useState<Chats[]>();
   const [sendMessage, { isLoading }] = useSendMessageMutation();
   const { register, handleSubmit, reset } = useForm<FormValue>();
